3b607d77cfea1d6fd1738e79a1309033ba1b563f
Sundeep Chind <sundeep.chind@mac.com>
2021-03-08 14:53:04 +0000

    Added create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,20 @@
 class ContactsController < ApplicationController
     def new
-      end
+      @contact = Contact.new  #The form attributes of name,email and comments saved to variable @contact
+    end
+    
+    def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+        redirect_to new_contact_path, notice: "Message sent."
+      else
+        redirect_to new_contact_path, notice: "Error occured"
+      end  
+    end  
+    
+    private #Security feature to whitelist the form attributes
+      def contact_params
+        params.require(:contact).permit(:name, :email, :comments) #White listing of form attributes 
+      end  
+    
 end
