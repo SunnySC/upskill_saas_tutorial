3972b9ba17f0e815e755b993f9392403c3ad6538
Sundeep Chind <sundeep.chind@mac.com>
2021-03-12 14:15:20 +0000

    Trigger the mailer from the controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,8 +4,12 @@ class ContactsController < ApplicationController
     end
     
     def create
-      @contact = Contact.new(contact_params)
+      @contact = Contact.new(contact_params)   #paramas specifies mass assignment
       if @contact.save
+        name = params[:contact][:name]
+        email = params[:contact][:email]
+        body = params[:contact][:comments]
+        ContactMailer.contact_email (name, email, body).deliver
         flash[:success] = "Message Sent"
         redirect_to new_contact_path
       else
@@ -16,7 +20,7 @@ class ContactsController < ApplicationController
     
     private #Security feature to whitelist the form attributes
       def contact_params
-        params.require(:contact).permit(:name, :email, :comments) #White listing of form attributes 
+        params.require(:contact).permit(:name, :email, :comments) #White listing of form attributes and called mass assignment 
       end  
     
 end
